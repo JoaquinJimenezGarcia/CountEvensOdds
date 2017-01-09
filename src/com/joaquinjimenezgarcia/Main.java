410e45786ac93b8fba81975855907d9a5f7dab7c
Joaquin Jimenez Garcia <garcia.jjimenez@gmail.com>
2017-01-09 16:42:32 +0100

    Commented

--- src/com/joaquinjimenezgarcia/Main.java
+++ src/com/joaquinjimenezgarcia/Main.java
@@ -6,41 +6,54 @@ public class Main {
 
     public static void main(String[] args) {
 
-        final int SIZE = 100;
-        final int ARRAY [] = new int[SIZE];
-        final int MAXIMUM = 99;
-        final int MINIMUM = 0;
+        final int SIZE = 100; // Size of the Array
+        final int ARRAY [] = new int[SIZE]; // Empty array for fill with the random number
+        final int MAX = 99; // Max limit of the number generated
+        final int MIN = 0; // Min limit of the nomber generated
 
-        fillArray(ARRAY, MAXIMUM, MINIMUM);
+        fillArray(ARRAY, MAX, MIN);
         printArray(ARRAY);
-        printCountTens(MAXIMUM, MINIMUM, ARRAY);
+        printCountTens(MAX, MIN, ARRAY);
         countEvensOdds(ARRAY, SIZE);
 
     }
 
-    static void fillArray(int arrayFilled[], int limitMaximum, int limitMinimum){
+    /**
+     * Fill the array with random values (min-max)
+     * @param arrayFilled
+     * @param limitMax
+     * @param limitMin
+     */
+    static void fillArray(int arrayFilled[], int limitMax, int limitMin){
         for (int i = 0; i < arrayFilled.length ; i++) {
-            arrayFilled[i] = (int)(Math.random()*limitMaximum+limitMinimum);
+            arrayFilled[i] = (int)(Math.random()*limitMax+limitMin);
         }
     }
 
+    /**
+     * Print the array with the random numbers (10 per line)
+     * @param printedArray
+     */
     static void printArray(int printedArray[]){
-        int lineUp = 0;
-
         System.out.println("NUMBERS");
         System.out.println("=======");
         for (int i = 0; i < printedArray.length; i++) {
             System.out.printf(" %2d", printedArray[i]);
-            lineUp++;
 
-            if (lineUp==10){
+            // Change the line when it has 10 numbers
+            if ((i+1)%10 == 0){
                 System.out.println();
-                lineUp = 0;
             }
         }
         System.out.println();
     }
 
+    /**
+     * Count and print how many numbers are per tens
+     * @param maxNumber
+     * @param minNumber
+     * @param arrayCount
+     */
     static void printCountTens(int maxNumber, int minNumber, int arrayCount[]){
         System.out.println("COUNT");
         System.out.println("=====");
@@ -64,8 +77,13 @@ public class Main {
         System.out.println();
     }
 
+    /**
+     * Count the porcentage of evens and odds and print it
+     * @param arrayEvensOdds
+     * @param total
+     */
     static void countEvensOdds(int arrayEvensOdds[], int total){
-        DecimalFormat decimals = new DecimalFormat("0.00");
+        DecimalFormat decimals = new DecimalFormat("0.00"); // Indicates how many decimals
 
         int evens = 0, odds = 0;
         final double porcentageEvens, porcentageOdds;
